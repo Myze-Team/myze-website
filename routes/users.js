5982887637c36c307cf08adb11fb1f480c815f91
Xianhai Cao <xianhaic@gmail.com>
2020-03-01 15:50:28 -0800

    Linked server to MongoDB and created registration route
    The MongoDB URI depends on environment variables
    initialized by dotenv. Make sure to have a .env file
    filled with values for MONGO_DB_USER, MONGO_DB_PASS,
    and MONGO_DB_CLUSTER. Login route with passport-jwt
    authentication is a work in progress.

---
+++ routes/users.js
@@ -0,0 +1,42 @@
+require('dotenv').config()
+const router = require('express').Router()
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+// load user model
+const User = require('../models/User')
+
+// load validation helpers
+const validator = require('../validation/validator')
+
+router.post('/register', (req, res) => {
+  const { errors, isValid } = validator.validateRegisterData(req.body)
+
+  if (!isValid)
+    return res.status(400).json(errors)
+
+  User.findOne({ email: req.body.email }).then(user => {
+    // check for uniqueness
+    if (user)
+      return res.status(400).json({ email: 'Email already exists' })
+    else {
+      // create a new object to save later
+      const newUser = new User({
+        email: req.body.name,
+        password: req.body.password,
+      })
+
+      // generate salted password hash
+      // we only store the hash, never the plain-text password
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err
+          newUser.password = hash
+          newUser.save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err))
+        })
+      })
+    }
+  })
+})
