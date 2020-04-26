db51eab46063ef88e6ad13b2bf9e364ee3ccc0d9
Paul Pan <panjunhong05@gmail.com>
2020-04-25 21:21:00 -0700

    Changed logo to svg

--- src/pages/navbar/index.tsx
+++ src/pages/navbar/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import styles from './index.module.scss';
-import MyzePng from '../../assets/img/myze.png';
+import MyzeSvg from '../../assets/img/myze.svg';
 
 const Navbar: React.FC = () => (
   // ${this.state.isTop ? styles.scrolled : ""}
   <div className={styles.nav}>
     <div className={`${styles.navbar} ${styles.responsiveContainer}`}>
-      <div className={styles.logoContainer}>
-        <img src={MyzePng} alt="Myze Logo" className={styles.myzeLogo} />
-        <div className={styles.logoText}>myze</div>
-      </div>
+      <img src={MyzeSvg} alt="Myze Logo" className={styles.myzeLogo} />
       <div className={styles.navbarList}>
         <div>How It Works</div>
         <div>About Us</div>
