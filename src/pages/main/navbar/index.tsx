049139153fd036f91590039329b06418cc7e83eb
linglingandy <zeusandninja@gmail.com>
2020-05-22 02:01:12 -0700

    made navbar list change color on scroll

--- src/pages/main/navbar/index.tsx
+++ src/pages/main/navbar/index.tsx
@@ -15,27 +15,56 @@ const scrollTo = (id: string) => {
   });
 };
 
-const Navbar: React.FC<NavbarProps> = ({ scrolled }) => (
-  <div className={`${styles.nav} ${scrolled}`}>
-    <div className={`${styles.navbar} responsiveContainer`}>
-      <button type="button" className={styles.logoContainer} onClick={() => scrollTo('cover')}>
-        <img src={MyzeSvg} alt="Myze Logo" className={styles.myzeLogo} />
-        <div className={styles.logoText}>myze</div>
-      </button>
-      <div className={styles.navbarList}>
-        <button type="button" onClick={() => scrollTo('how-it-works')}>
-          How It Works
+const Navbar: React.FC<NavbarProps> = ({ scrolled }) => {
+  const [active, setActive] = React.useState('cover');
+  const handleActive = React.useCallback(() => {
+    var section = document.getElementById("root")?.getElementsByClassName("responsiveContainer") as HTMLCollectionOf<HTMLElement>;
+    var sections: { [key: string]: any } = {};
+    if(section != null) {
+      for(var i = 1; i < section.length; i++) {
+          sections[section[i].id] = section[i].offsetHeight;
+      }
+      var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+      var total = 0;
+      var s: string;
+      for (s in sections) {
+        if (currentScroll > total - 300 && currentScroll < total + sections[s] - 300) {
+          setActive(s);
+        }
+        total = total + sections[s];
+      }
+    }
+  }, []);
+  React.useEffect(() => {
+    window.addEventListener('scroll', handleActive);
+    return () => {
+      window.removeEventListener('scroll', handleActive);
+    };
+  }, [handleActive]);
+  return (
+    <div className={`${styles.nav} ${scrolled}`}>
+      <div className={`${styles.navbar} responsiveContainer`}>
+        <button type="button" className={styles.logoContainer} onClick={() => scrollTo('cover')}>
+          <img src={MyzeSvg} alt="Myze Logo" className={styles.myzeLogo} />
+          <div className={styles.logoText}>myze</div>
         </button>
-        <button type="button" onClick={() => scrollTo('about-us')}>
-          About Us
-        </button>
-        <button type="button">Contact Us</button>
-        <NavLink to="#login" className={styles.loginButton}>
-          Sign In
-        </NavLink>
+        <div className={styles.navbarList}>
+          <button type="button" className={`${active}` == 'cover' ? `${styles.active}` : `${styles.inactive}`} onClick={() => {scrollTo('cover')}}>
+            Home
+          </button>
+          <button type="button" className={`${active}` == 'how-it-works' ? `${styles.active}` : `${styles.inactive}`} onClick={() => {scrollTo('how-it-works')}}>
+            How It Works
+          </button>
+          <button type="button" className={`${active}` == 'about-us' ? `${styles.active}` : `${styles.inactive}`} onClick={() => {scrollTo('about-us')}}>
+            About Us
+          </button>
+          <button type="button">Contact Us</button>
+          <NavLink to="#login" className={styles.loginButton}>
+            Sign In
+          </NavLink>
+        </div>
       </div>
     </div>
-  </div>
-);
-
+  );
+};
 export default Navbar;
